75c3986e3f46318e1677164c0726ea781fe539da
Marco Rossi <marco.rossi1337@gmail.com>
2017-06-08 16:19:19 +0200

    Updated Copyright Header to match LGPL standards

--- PVSystem/emoncms/jsonparser.cpp
+++ PVSystem/emoncms/jsonparser.cpp
@@ -1,11 +1,26 @@
-/*******************************************************
- * Copyright (C) 2017-2018 Marco Rossi marco.rossi1337@gmail.com
+/****************************************************************************
  *
- * This file is part of PVSystem.
+ *   Copyright (C) 2017 Marco Rossi. All rights reserved.
+ *   Author: Marco Rossi <info@nerinformatica.it>
  *
- * PVSystem can not be copied and/or distributed without the express
- * permission of Marco Rossi
- *******************************************************/
+ *   Supervisor: Luciano Neri <l.neri@nerinformatica.it>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the LGPL v3 license
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
 
 #include "jsonparser.h"
 #include <QObject>
